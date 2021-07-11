ce46f77fe0658e62645ce423aa345707edf2ad1d
Cody Platt <62489180+plattco@users.noreply.github.com>
2021-07-10 21:49:31 -0400

    added vuerouter

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        projects: [],
-        perPage: 20,
-        page: 1
-    },
+
+const Home = {template: '<div>My Portfolio</div>'} 
+const Projects = {
+    
+    template: `<div> 
+         <div v-for="project in projects">
+            <h2 class="title">{{project.full_name}}</h2>
+            
+            <div class="author">
+                <img width="50px" :src="project.owner.avatar_url" alt="me">
+            </div>
+            <div class="view">
+                <a :href="project.html_url">View</a>
+            </div>
+        </div>
+    </div>`,
+    data(){
+        return {
+            projects: [],
+            perPage: 20,
+            page: 1
+        }
+    }, 
     mounted(){
-        // Api call
-
-        axios.get(`https://api.github.com/users/plattco/repos?per_page=${this.perPage}&page=${this.page}`)
-        .then(response => { 
-            console.log(response);
-            this.projects = response.data;
-        }).catch(error => { 
-            console.log(error);
-        })
-
+        
+         axios
+         .get(`https://api.github.com/users/plattco/repos?per_page=${this.perPage}&page=${this.page}`)
+         .then(
+            response => {
+                //console.log(response);
+                this.projects = response.data;
+            }
+        )
+        .catch(error=> {console.log(error);})
     }
-})
+} 
+
+// Define some routes
+const routes = [
+    {path: '/', component: Home},
+    {path: '/projects', component: Projects}
+];
+// Create the router instance and pass the routes to it
+const router = new VueRouter({
+routes: routes
+});
+// Create and mount the root instance.
+
+let app = new Vue({
+    router 
+}).$mount('#app');
