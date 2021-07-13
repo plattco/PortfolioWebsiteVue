490b5bd2f4785b884f74791d8a3def3e4147ee6c
Cody Platt <62489180+plattco@users.noreply.github.com>
2021-07-13 17:11:23 -0400

    fixed github link

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ const Projects = {
                             <button class="btn_load_more" v-on:click="loadMore()">Load More</button>
                         </div>
                         <div v-else>
-                            <a href="" target="_blank" rel="noopener noreferrer">Visit My GitHub</a>
+                            <a href="wwww.github.com/plattco" target="_blank" rel="noopener noreferrer">Visit My GitHub</a>
                         </div>
 
                     </div>
